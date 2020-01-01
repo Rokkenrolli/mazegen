e72d0b78a7588e5dbc4c662b7363a0cc1d38be72
Olli <olli.e.oksanen@aalto.fi>
2020-01-01 19:20:04 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mazegen/precache-manifest.6d730aaf759f7007b10a43b3635d9861.js"
+  "/mazegen/precache-manifest.260dbb41518d16876a59e7b7a766b246.js"
 );
 
 self.addEventListener('message', (event) => {
