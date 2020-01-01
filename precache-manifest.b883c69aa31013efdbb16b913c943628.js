c17a54dd1e43ba006ab7ca5dc9c259140c1be205
Olli <olli.e.oksanen@aalto.fi>
2020-01-01 18:44:59 +0200

    Updates

---
+++ precache-manifest.b883c69aa31013efdbb16b913c943628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ce1750b632d4d3972f64f4bab3c179",
+    "url": "/mazegen/index.html"
+  },
+  {
+    "revision": "ca875b272045828123d9",
+    "url": "/mazegen/static/css/main.673c74e5.chunk.css"
+  },
+  {
+    "revision": "41120b751c33af5260d5",
+    "url": "/mazegen/static/js/2.73e6054b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mazegen/static/js/2.73e6054b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca875b272045828123d9",
+    "url": "/mazegen/static/js/main.ced72dcc.chunk.js"
+  },
+  {
+    "revision": "e8861a2557833cda6993",
+    "url": "/mazegen/static/js/runtime-main.a74aacca.js"
+  }
+]);
